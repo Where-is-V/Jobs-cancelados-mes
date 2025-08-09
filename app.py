5078220065dc0a3be7555744136bf4316c19701a
V <106013925+Where-is-V@users.noreply.github.com>
2025-08-09 02:33:39 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# --- Configuração da Página ---
+# Define o título da página, o ícone e o layout para ocupar a largura inteira.
+st.set_page_config(
+    page_title="Dashboard de Jobs cancelados por mês",
+    page_icon="📊",
+    layout="wide",
+)
